366a14f93030f59c9de95440ef5b39ad35b22e6c
Jekel8888 <52115322+Jekel8888@users.noreply.github.com>
2020-01-29 09:28:40 -0700

    baseline version

---
+++ dsa-search-algorithms/src/Drills.js
@@ -0,0 +1,15 @@
+// 1. How many searches?
+// Given a sorted list 3, 5, 6, 8, 11, 12, 14, 15, 17, 18 and using the recursive binary search algorithm, identify the sequence of numbers that each recursive call will search to try and find 8.
+
+// Given a sorted list 3, 5, 6, 8, 11, 12, 14, 15, 17, 18 and using the recursive binary search algorithm, identify the sequence of numbers that each recursive call will search to try and find 16.
+
+// 3,5,6,8,11,12,14,15,17,18
+
+// Pt 1
+// root = 11
+// 11, 5, 6, 8
+
+// Pt 2
+// root = 11
+// 16 is > 11
+// 11, 15, 17 no 16;
